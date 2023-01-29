d10948127a021f69fea2fb826a8abe44e5c5965d
Andres Luna <andreslunacas2000@gmail.com>
2023-01-29 16:52:36 -0400

    agregue las validaciones al html

---
+++ src/login.js
@@ -0,0 +1,11 @@
+export default function errorForm($buttonLogin,$userLogin,$passwordLogin,$errorMessage) {
+  const d = document;
+
+  d.addEventListener("click", e => {
+    if (e.target.matches($buttonLogin)) {
+      d.getElementById("")
+    }
+  })
+
+}
+
